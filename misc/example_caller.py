6b379d62b4c619a4f824f9e67494353c2e3542ff
Ciaran Doherty <cad106uk@gmail.com>
2015-08-23 18:30:37 +0100

    Example FFI client.

---
+++ misc/example_caller.py
@@ -0,0 +1,30 @@
+import threading
+from ctypes import CDLL
+
+
+def worker(val):
+    count = 0
+    while count < 15000000:
+        count += 1
+
+    print val, count
+
+
+def main():
+    lib = CDLL(
+        "/home/ciaran/Documents/rust/web_scraper/"
+        "target/release/libweb_scraper.so"
+    )
+    lib.process()
+    # threads = []
+    # for i in range(10):
+    #     t = threading.Thread(target=worker, args=(i,))
+    #     threads.append(t)
+    #     t.start()
+
+    # for t in threads:
+    #     t.join()
+
+
+if __name__ == "__main__":
+    main()
