4944056bfc2c53fedc44c99becb7c63699689f10
Ciaran Doherty <cad106uk@gmail.com>
2015-08-23 18:24:24 +0100

    First working example:
    Micky Mouse example of an FFI call with test.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,28 @@
+use std::thread;
+
+fn worker() -> i64 {
+    let mut x: i64 = 0;
+    while x < 15_000_000 {
+        x += 1
+    }
+    x
+}
+
+#[no_mangle]
+pub extern fn process() {
+    let handles: Vec<_> = (0..10).map(
+        |_| {thread::spawn(|| worker())
+    }).collect();
+
+    for h in handles {
+        let res = h.join().map_err(|_| "Could not join a thread!");
+        println!("Thread finished with count={}", res.unwrap());
+    }
+    println!("Done");
+}
+
+
 #[test]
 fn it_works() {
+    assert_eq!(worker(), 15_000_000);
 }
