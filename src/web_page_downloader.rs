e3665e727c5ffd987febe3349066e6204454f9b6
Ciaran Doherty <Ciaran.Doherty@cyantechnology.com>
2016-01-18 16:05:49 +0000

    Starting a website crawler.

--- src/web_page_downloader.rs
+++ src/web_page_downloader.rs
@@ -2,22 +2,8 @@
 The function store_raw_html_page is usually the only function needed
 I have however left the download_page function public just encase
 */
-extern crate hyper;
-
 use std::io::Read;
 use std::sync::mpsc::Sender;
-
-use hyper::Client;
-use hyper::header::Connection;
-
-pub fn download_page(url: String) -> String {
-    let client = Client::new();
-    let mut res = client.get(&url[..]).header(Connection::close()).send().unwrap();
-
-    let mut body = String::new();
-    res.read_to_string(&mut body).unwrap();
-    body
-}
 
 pub fn store_raw_html_page(pages: Sender<String>, thread_url: String) {
     pages.send(download_page(thread_url)).unwrap()
---
+++ src/website_crawler.rs
@@ -0,0 +1,59 @@
+// Given an URL, get the page for that URL
+// Given a page of a website, get all the anchors that point to pages within that website
+// Make sure that each intrnal anchor is only recorded once.
+// visit ever url in the website record all the and pages and urls
+
+use std::collections::{HashMap, HashSet};
+use std::sync::mpsc::{channel, Receiver};
+
+use hyper::Client;
+use hyper::header::Connection;
+
+use html5ever::{parse, one_input};
+use html5ever::rcdom::Element;
+
+use url::Url;
+
+let mut KnownWebsitePages = HashMap::new();
+
+fn crawl_page(page_url: String) {
+    let parsed_url = Url::parse(page_url).unwrap();
+
+    let mut pages = KnownWebsitePagess.entry(
+        parsed_url.domain().unwrap()
+    ).or_insert(mut HashSet::new());
+
+    if pages.insert(parsed_url) {
+    }
+}
+
+fn handle_node_anchor(node: Element) {
+    //Your moma
+}
+
+fn get_internal_links(page: RcDom) {
+    let node = handle.borrow();
+
+    match node.node {
+        Element(ref nmae, _ _) => {
+            if name.local == Atom::from("a") {
+            }
+        },
+
+        _ => ()
+    },
+}
+
+fn download_page(url: String) -> RcDom {
+    let client = Client::new();
+    let res = client.get(&url[..]).header(Connection::close()).send().unwrap();
+
+    let mut body = String::new();
+    let mut input = StrTendril::new();
+    res.read_to_string(&mut body).unwrap();
+    input.try_push_bytes(body.as_bytes());
+
+    let parsed_dom: RcDom = parse(one_input(input));
+    get_internal_links(parsed_dom);
+    parsed_dom
+}
