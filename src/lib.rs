c6edd767c086c0395ffc0dbd1ab85469a66ff624
Ciaran Doherty <Ciaran.Doherty@cyantechnology.com>
2016-02-02 10:41:14 +0000

    backup

--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,13 @@ extern crate string_cache;
 extern crate hyper;
 extern crate url;
 
+use std::collections::{HashMap, HashSet, VecDeque};
 use std::result::Result;
 use std::thread;
 use std::string::String;
 use std::ffi::CStr;
 use std::default::Default;
+use std::sync::Arc;
 use std::sync::mpsc::{channel, Receiver};
 use libc::c_char;
 
@@ -20,9 +22,115 @@ use tendril::StrTendril;
 
 use string_cache::Atom;
 
+use url::Url;
+
 use website_crawler::{download_page, store_raw_html_page};
 
 mod website_crawler;
+
+
+struct ThreadCoordinator {
+    KnownWebsitePages: HashMap<String, HashSet<String>>,
+    PagesToDownload: VecDeque<Url>,
+    PagesToProcess: VecDeque<&String>,
+    ParsedPagesForInternalAchors: VecDeque<&RcDom>,
+    PagesToParse: VecDeque<&RcDom>,
+}
+
+impl ThreadCoordinator {
+    fn get_page_if_needed(&self, url: String) {
+        let page = match Url::parse(&url) {
+            Ok(is_page) => is_page,
+            Err(_) => return,
+        };
+
+        let mut visited_pages = match page.domain() {
+            Some(domain) => {
+                match self.KnownWebsitePages.get(domain) {
+                    Some(pages) => pages,
+                    None => return,
+                }
+            },
+            None => return,
+        };
+
+        let path = match page.path() {
+            Some(path_vec) => path_vec.join("/"),
+            None => return,
+        };
+        if visited_pages.insert(path) {
+            self.PagesToDownload.push_front(page);
+        }
+    }
+}
+
+fn start_read_thread(url: String) {
+    // have multiple threads do this.
+    // record url
+    // download page
+    // parse page
+    // get all internal urls
+    // for each new url goto record url step
+    let mut tmp = Arc::new(Box::new(ThreadCoordinator {
+        KnownWebsitePages: HashMap::new(),
+        PagesToDownload: VecDeque::new(),
+    }));
+    let domain = String::from(Url::parse(&url).unwrap().domain().unwrap());
+
+    // KnownWebsitePages.insert(domain, HashSet::new());
+
+    // match KnownWebsitePages.get(&domain) {
+    //     Some(pages) => {
+    //         pages.insert("".to_string());
+    //         pages.insert("/".to_string());
+    //     },
+    //     _ => ()
+    // }
+    let (page_to_download, download_page) = channel();
+
+    let (page_to_parse, parse_page) = channel();
+    let (sender, receiver) = channel::<String>();
+    let thread_url = url.clone().to_string();
+
+    let process_page = thread::spawn(|| process_next_page(receiver));
+    thread::spawn(|| store_raw_html_page(sender, thread_url));
+
+    let res = process_page.join();
+    match res {
+        Ok(v) => println!("Thread finished with count={:?}", v),
+        Err(e) => println!("Thread errored with count={:?}", e),
+    };
+}
+
+#[no_mangle]
+pub extern "C" fn process(url: *const c_char) {
+    let c_value = Some(unsafe { CStr::from_ptr(url).to_string_lossy().into_owned() });
+
+    match c_value {
+        Some(value) => start_read_thread(String::from(&value[..])),
+        None => (),
+    }
+}
+
+
+#[test]
+fn it_works() {
+    let raw_html_page = download_page("http://slashdot.org".to_string());
+    let mut input = StrTendril::new();
+    let _ = input.try_push_bytes(raw_html_page.as_bytes());
+
+    let dom: RcDom = parse(one_input(input), Default::default());
+    let mut output = Box::new(0u64);
+    let res = walk(dom.document, &mut output);
+    assert_eq!(*output, 26);
+    assert!(res.is_ok());
+    assert_eq!(match res {
+                   Ok(val) => val,
+                   Err(e) => e,
+               },
+               26);
+}
+
 
 fn walk(handle: Handle, count: &mut Box<u64>) -> Result<u64, u64> {
     let node = handle.borrow();
@@ -68,85 +176,3 @@ fn process_next_page(raw_pages: Receiver<String>) -> Result<u64, u64> {
     println!("Output {:?}", output);
     res
 }
-
-fn start_read_thread(url: String) {
-    let (sender, receiver) = channel::<String>();
-    let thread_url = url.clone().to_string();
-
-    let process_page = thread::spawn(|| process_next_page(receiver));
-    thread::spawn(|| store_raw_html_page(sender, thread_url));
-
-    let res = process_page.join();
-    match res {
-        Ok(v) => println!("Thread finished with count={:?}", v),
-        Err(e) => println!("Thread errored with count={:?}", e),
-    };
-}
-
-#[no_mangle]
-pub extern "C" fn process(url: *const c_char) {
-    let c_value = Some(unsafe { CStr::from_ptr(url).to_string_lossy().into_owned() });
-
-    match c_value {
-        Some(value) => start_read_thread(String::from(&value[..])),
-        None => (),
-    }
-}
-
-
-#[test]
-fn it_works() {
-    let raw_html_page = download_page("http://slashdot.org".to_string());
-    let mut input = StrTendril::new();
-    let _ = input.try_push_bytes(raw_html_page.as_bytes());
-
-    let dom: RcDom = parse(one_input(input), Default::default());
-    let mut output = Box::new(0u64);
-    let res = walk(dom.document, &mut output);
-    assert_eq!(*output, 26);
-    assert!(res.is_ok());
-    assert_eq!(match res {
-                   Ok(val) => val,
-                   Err(e) => e,
-               },
-               26);
-}
-
-// fn start_read_thread(url: String) {
-//     let thread_url = url.clone().to_string();
-//     let handles: Vec<_> = (0..2).map(move |_| {
-//         thread::spawn(|| {
-//             let raw_html_page = worker(thread_url);
-//             let mut input = StrTendril::new();
-//             let _ = input.try_push_bytes(raw_html_page.as_bytes());
-
-//             let dom: RcDom = parse(one_input(input), Default::default());
-//             walk(0, dom.document);
-//         })}).collect();
-
-//     for h in handles {
-//         let res = h.join();
-//         match res {
-//             Ok(v) => println!("Thread finished with count={:?}", v),
-//             Err(e) => println!("Thread errored with count={:?}", e),
-//         }
-
-//     }
-//     println!("Done");
-// }
-
-
-// fn run_in_this_thread(url: String) {
-//     let raw_html_page = worker(url.to_string());
-//     let mut input = StrTendril::new();
-//     let _ = input.try_push_bytes(raw_html_page.as_bytes());
-
-//     let dom: RcDom = parse(one_input(input), Default::default());
-//     let mut output = Box::new(0u64);
-//     let res = walk(0, dom.document, &mut output);
-//     match res {
-//         Ok(v) => println!("Thread finished with count={:?} output={:?}", v, output),
-//         Err(e) => println!("Thread errored with count={:?} output={:?}", e, output),
-//     }
-//     println!("Done");
-// }
--- src/website_crawler.rs
+++ src/website_crawler.rs
@@ -1,4 +1,3 @@
-
 use std::collections::{HashMap, HashSet};
 use std::sync::mpsc::{channel, Receiver};
 use std::sync::mpsc::Sender;
@@ -17,19 +16,18 @@ use html5ever::rcdom::{Element, RcDom};
 
 use url::Url;
 
-static mut KnownWebsitePages: HashMap<String, HashSet<String>> = HashMap::new();
 
-fn crawl_page(page_url: String) {
-    let parsed_url = String::from(Url::parse(&page_url).unwrap().domain().unwrap());
+// fn crawl_page(page_url: String) {
+//     let parsed_url = String::from(Url::parse(&page_url).unwrap().domain().unwrap());
 
-    unsafe {
-        let mut pages = KnownWebsitePages.entry(parsed_url)
-                                         .or_insert(HashSet::new());
+//     unsafe {
+//         let mut pages = KnownWebsitePages.entry(parsed_url)
+//                                          .or_insert(HashSet::new());
 
-        if pages.insert(parsed_url) {
-        }
-    }
-}
+//         if pages.insert(parsed_url) {
+//         }
+//     }
+// }
 
 // fn handle_node_anchor(node: Element) {
 //     //Your moma
@@ -57,12 +55,18 @@ fn download_page(url: String) -> String {
     let res = client.get(&url[..]).header(Connection::close()).send().unwrap();
 
     let mut body = String::new();
-    let mut input = StrTendril::new();
     res.read_to_string(&mut body).unwrap();
+}
+
+fn parse_page(body: &String) -> RcDom {
+    let mut input = StrTendril::new();
     input.try_push_bytes(body.as_bytes());
 
-    let parsed_dom: RcDom = parse(one_input(input), Default::default());
-    get_internal_links(parsed_dom);
-    body
+    parse(one_input(input), Default::default())
+}
+
+fn crawl_site_from_page(parsed_dom: &RcDom) {
+    get_internal_links(*parsed_dom);
+    // body
     // parsed_dom
 }
