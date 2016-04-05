fa5c9f30ad2e61a2eadcaddfed7f7cabb702d69e
Ciaran Doherty <cad106uk@gmail.com>
2016-04-05 22:34:55 +0100

    Clean up task queues.
    Using rustfmt.

--- src/task_queue.rs
+++ src/task_queue.rs
@@ -1,7 +1,8 @@
 // The plan here is to have a set of tasks sitting of a FIFO queue
 // each thread gets 1 task and processes it. Each new task that is created
 // is added to the queue.
-//
+
+#![feature(braced_empty_structs)]
 
 use std::collections::VecDeque;
 use std::sync::mpsc::Sender;
@@ -10,12 +11,20 @@ use std::thread::{current, park, Thread};
 use std::vec::Vec;
 
 pub enum ProcessOutputs {
-    Processes(Vec<Box<AtomicProcess>>),
+    Processes(Box<AtomicProcess>),
     Output(i64),
 }
 
 trait AtomicProcess {
     fn process_this(&self) -> ProcessOutputs;
+}
+
+struct PauseThread;
+impl AtomicProcess for PauseThread {
+    fn process_this(&self) -> ProcessOutputs {
+        park();
+        ProcessOutputs::Output(0i64)
+    }
 }
 
 struct TaskQueue {
@@ -25,25 +34,24 @@ struct TaskQueue {
 }
 
 impl TaskQueue {
-    pub fn nextTask(&self) -> Option<AtomicProcess> {
+    pub fn nextTask(&self) -> Box<AtomicProcess> {
         match self.queue.clone().lock().unwrap().pop_back() {
-            Some(res) => &res,
-            None => park(current()),
+            Some(res) => res,
+            None => Box::new(PauseThread),
         }
     }
 
     pub fn addTask(&self, new_task: Box<AtomicProcess>) {
         self.queue.clone().lock().unwrap().push_front(new_task);
-        self.threads.clone().lock().unwrap().iter().map(|t| t.unpark())
+        self.threads.clone().lock().unwrap().iter().map(|t| t.unpark());
     }
 
     pub fn addThreadToWorkers(&self, t_handle: Thread) {
         self.threads.clone().lock().unwrap().push(t_handle);
         loop {
-            let task = self.nextTask();
-            match task.process_this() {
-                ProcessOutputs::Processes(res) => res.iter().map(|res| self.addTask(res)),
-                ProcessOutputs::Output(output) => self.output_channel.send(output),
+            match self.nextTask().process_this() {
+                ProcessOutputs::Processes(res) => self.addTask(res),
+                ProcessOutputs::Output(res) => self.output_channel.send(res).unwrap(),
             }
         }
     }
