39b35981c9c3531c8d4a8ce88790e5018450b0dc
Ciaran Doherty <cad106uk@gmail.com>
2016-04-27 09:28:30 +0100

    I took the advice from rustfmt and put the code into 1 file.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#![feature(braced_empty_structs)]
 extern crate html5ever;
 extern crate hyper;
 extern crate libc;
@@ -5,40 +6,169 @@ extern crate string_cache;
 extern crate tendril;
 extern crate url;
 
-use std::collections::{HashMap, HashSet, VecDeque};
-use std::result::Result;
-use std::thread;
-use std::string::String;
-use std::ffi::CStr;
-use std::default::Default;
-use std::sync::mpsc::{channel, Receiver, Sender};
-use std::sync::{Arc, Mutex};
 use libc::c_char;
 
-use html5ever::parse_document;
-use tendril::StrTendril;
+use std::collections::{HashMap, HashSet, VecDeque};
+use std::default::Default;
+use std::ffi::CStr;
+use std::io::Read;
+use std::result::Result;
+use std::string::String;
+use std::sync::{Arc, Mutex};
+use std::sync::mpsc::{channel, Receiver, Sender};
+use std::thread;
+use std::thread::{current, park, Thread};
 
+use tendril::StrTendril;
 use string_cache::Atom;
 
-use url::Url;
-use task_queue::{TaskQueue, AtomicProcess};
+use hyper::Client;
+use hyper::header::Connection;
 
-mod website_crawler;
-mod task_queue;
+use html5ever::parse_document;
+use html5ever::tree_builder::TreeBuilderOpts;
+use html5ever::driver::{ParseOpts, BytesOpts};
+use html5ever::rcdom::{Document, Doctype, Comment, Element, RcDom, Handle, Text};
+
+use url::Url;
+
+pub enum ProcessOutputs {
+    Processes(Box<AtomicProcess>),
+    Output(i64),
+}
+
+trait AtomicProcess {
+    fn process_this(&self) -> Vec<ProcessOutputs>;
+}
+
+struct TaskQueue {
+    threads: Arc<Mutex<Vec<Thread>>>,
+    queue: Arc<Mutex<VecDeque<Box<AtomicProcess>>>>,
+    output_channel: Sender<i64>,
+}
+
+struct PageDownloader {
+    thread_url: String,
+}
+
+struct WalkDom {
+    handle: Handle,
+    count: i64,
+}
+
+struct PauseThread;
+// The plan here is to have a set of tasks sitting of a FIFO queue
+// each thread gets 1 task and processes it. Each new task that is created
+// is added to the queue.
+
+impl AtomicProcess for PauseThread {
+    fn process_this(&self) -> Vec<ProcessOutputs> {
+        park();
+        vec![ProcessOutputs::Output(0i64)]
+    }
+}
+
+impl TaskQueue {
+    pub fn nextTask(&self) -> Box<AtomicProcess> {
+        match self.queue.clone().lock().unwrap().pop_back() {
+            Some(res) => res,
+            None => Box::new(PauseThread),
+        }
+    }
+
+    pub fn addTask(&self, new_task: Box<AtomicProcess>) {
+        self.queue.clone().lock().unwrap().push_front(new_task);
+        self.threads.clone().lock().unwrap().iter().map(|t| t.unpark());
+    }
+
+    pub fn addThreadToWorkers(&self, t_handle: Thread) {
+        self.threads.clone().lock().unwrap().push(t_handle);
+        loop {
+            for process in self.nextTask().process_this() {
+                match process {
+                    ProcessOutputs::Processes(res) => self.addTask(res),
+                    ProcessOutputs::Output(res) => self.output_channel.send(res).unwrap(),
+                }
+            }
+        }
+    }
+}
+
+impl AtomicProcess for WalkDom {
+    fn process_this(&self) -> Vec<ProcessOutputs> {
+        let node = self.handle.borrow();
+
+        match node.node {
+            Element(ref name, _, _) => {
+                if name.local == Atom::from("article") {
+                    self.count += 1
+                }
+            }
+
+            Document => (),
+
+            Doctype(_, _, _) => (),
+
+            Text(_) => (),
+
+            Comment(_) => (),
+        };
+
+        let dom_steps: Vec<ProcessOutputs> = node.children
+                                                 .iter()
+                                                 .map(|child| {
+                                                     ProcessOutputs::Processes(Box::new(WalkDom {
+                                                         handle: *child,
+                                                         count: self.count,
+                                                     }))
+                                                 })
+                                                 .collect();
+        if dom_steps.len() == 0 {
+            dom_steps.push(ProcessOutputs::Output(self.count));
+        }
+        dom_steps
+    }
+}
+
+impl AtomicProcess for PageDownloader {
+    fn process_this(&self) -> Vec<ProcessOutputs> {
+        let client = Client::new();
+        let res = client.get(&self.thread_url[..])
+                        .header(Connection::close())
+                        .send()
+                        .unwrap();
+
+        // Read the Response.
+        let mut body = String::new();
+
+        let mut input = StrTendril::new();
+        let _ = input.try_push_bytes(body.as_bytes());
+        let mut dom = parse_document(RcDom::default(), Default::default())
+                          .from_utf8()
+                          .process(input);
+
+
+        vec![ProcessOutputs::Processes(Box::new(WalkDom {
+                 handle: dom.document,
+                 count: 0i64,
+             }))]
+    }
+}
 
 fn start_read_thread(url: String) {
     let (sender, receiver) = channel::<i64>();
     let queue_controller = TaskQueue {
         threads: Arc::new(Mutex::new(vec![])),
-        queue:
-            Arc::new(Mutex::new(VecDeque::new())),
+        queue: Arc::new(Mutex::new(VecDeque::new())),
         output_channel: sender,
     };
-    (0..4).map(|_| {
-        let worker_thread = thread::spawn(move || {});
-        queue_controller.addThreadToWorkers(*worker_thread.thread());
-    }).collect();
-    queue_controller.addTask(Box::new(website_crawler::PageDownloader { thread_url: url }));
+    (0..4)
+        .map(|_| {
+            let worker_thread = thread::spawn(move || {});
+            queue_controller.addThreadToWorkers(*worker_thread.thread());
+        })
+        .collect();
+    queue_controller.addTask(Box::new(PageDownloader { thread_url: url }));
 
     loop {
         println!("Thread finished with count={:?}", receiver.recv().unwrap());
@@ -54,67 +184,3 @@ pub extern "C" fn process(url: *const c_char) {
         None => (),
     }
 }
-
-
-// #[test]
-// fn it_works() {
-//     let raw_html_page = download_page("http://slashdot.org".to_string());
-//     let mut input = StrTendril::new();
-//     let _ = input.try_push_bytes(raw_html_page.as_bytes());
-
-//     let dom: RcDom = parse(one_input(input), Default::default());
-//     let mut output = Box::new(0u64);
-//     let res = walk(dom.document, &mut output);
-//     assert_eq!(*output, 26);
-//     assert!(res.is_ok());
-//     assert_eq!(match res {
-//                    Ok(val) => val,
-//                    Err(e) => e,
-//                },
-//                26);
-// }
-
-
-// fn walk(handle: Handle, count: &mut Box<u64>) -> Result<u64, u64> {
-//     let node = handle.borrow();
-
-//     match node.node {
-//         Element(ref name, _, _) => {
-//             if name.local == Atom::from("article") {
-//                 **count += 1
-//             }
-//         }
-
-// Document => (),
-
-// Doctype(_, _, _) => (),
-
-// Text(_) => (),
-
-//         Comment(_) => (),
-//     };
-
-
-//     for child in node.children.iter() {
-//         let res = walk(child.clone(), count);
-
-//         match res {
-//             Ok(v) => println!("walk success with ={:?}", v),
-
-//             Err(e) => println!("walk errored with ={:?}", e),
-//         }
-//     }
-//     Ok(**count)
-// }
-// fn process_next_page(raw_pages: Receiver<String>) -> Result<u64, u64> {
-//     let raw_html_page = raw_pages.recv().unwrap();
-
-//     let mut output = Box::new(0u64);
-//     let mut input = StrTendril::new();
-//     let _ = input.try_push_bytes(raw_html_page.as_bytes());
-//     let page: RcDom = parse(one_input(input), Default::default());
-
-//     let res = walk(page.document, &mut output);
-//     println!("Output {:?}", output);
-//     res
-// }
